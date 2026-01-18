c628e1f8584f2ae3337534239db4b2115165e1fe
zzahkaboom24 <github@zzahkaboom24.de>
2026-01-18 05:33:37 +0100

    Update entrypoint.sh

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -109,13 +109,13 @@ train_aot() {
 		fi
 	done
 
-	
+	kill -TERM "$PID"
 	echo -e "Training finished. Waiting for creation of AOT cache file..."
 	while [[ ! -f "./Server/HytaleServer.aot" ]]; do
     	sleep 1
 	done
 	echo -e "AOT cache created: HytaleServer.aot. Restarting server..."
-	kill -TERM "$PID"
+	wait "$PID"
 	exec 3<&-
 }
 
